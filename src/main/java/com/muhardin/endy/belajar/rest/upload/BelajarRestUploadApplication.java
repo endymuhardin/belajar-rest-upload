3cee5b365baccddd064df750b4ff571789d9aed7
Endy Muhardin <endy.muhardin@gmail.com>
2023-02-08 20:54:41 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/rest/upload/BelajarRestUploadApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.rest.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarRestUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarRestUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhardin/endy/belajar/rest/upload/BelajarRestUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.rest.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarRestUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
